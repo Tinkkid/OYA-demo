65a2cbc8bfd08e41b5fb7aba25e62edeb6ca124f
Yuliia Kostovynska <y.kostovynska@gmail.com>
2022-11-28 10:57:55 +0100

    test

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  // Change code below this line
+   const isMatch = SAVED_PASSWORD;
+   if (isMatch === SAVED_PASSWORD)
+      
+   
+
+  // Change code above this line
+  return isMatch;
+}
+isValidPassword("mangodab3st");
+isValidPassword("kiwirul3z");
+isValidPassword("jqueryismyjam");
